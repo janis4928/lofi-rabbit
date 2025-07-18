fadff697a6c14b90c8e4d78a970fde2837b208f8
unknown <email@example.com>
2025-07-18 17:54:42 +0800

    feat: replace homepage with background and music player

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@
 module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}', // 如果你有 pages 資料夾
   ],
   theme: {
     extend: {},
